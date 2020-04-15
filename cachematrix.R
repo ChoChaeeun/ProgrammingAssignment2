b2855db86e673d86ede96404413455c953c190eb
ChoChaeeun <chochaeeun@gist.ac.kr>
2020-04-16 01:21:08 +0900

    Update cachematrix.R
    lexial scoping_inverse not repeating

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## memorizing and making matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  n<-NULL
+  set<-function(z){
+        x<<-z
+        n<<-NULL
+  }
+  get<-function()x
+  setInverse<-function(inverse)n<<-inverse
+  getInverse<-function()n
+  list(set=set,get,get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+## do not repeat if is right
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    n<-x$getInverse()
+    if(!is.null(n)){
+      message("getting cached data")
+      return(n)
+    }
+    q<-x$get()
+    n<-solve(q,...)
+    x$setInverse(n)
+    n
 }
