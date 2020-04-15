506d95c3d87451fd9b4845e4495d02e0c6dbcfa5
ChoChaeeun <chochaeeun@gist.ac.kr>
2020-04-16 01:38:10 +0900

    Lexial scoping_edit

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacherMatrix
+## inverse
+## for initial matrix, inverse to NULL
+## already inversed, doing nothing
 ## memorizing and making matrix
 makeCacheMatrix <- function(x = matrix()) {
   n<-NULL
+  
   set<-function(z){
         x<<-z
         n<<-NULL
   }
+  
   get<-function()x
-  setInverse<-function(inverse)n<<-inverse
+  setInverse<-function(invs)n<<-invs
   getInverse<-function()n
   list(set=set,get,get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-## do not repeat if is right
+## cacheSolve
+## already in, this function find from the cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     n<-x$getInverse()
@@ -25,6 +30,7 @@ cacheSolve <- function(x, ...) {
       message("getting cached data")
       return(n)
     }
+    
     q<-x$get()
     n<-solve(q,...)
     x$setInverse(n)
